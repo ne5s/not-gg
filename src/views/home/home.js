0fd8031a9792c88e5cb17279b7abd5142aaf91e9
okdol1 <okdol0611@gmail.com>
2022-06-26 20:25:58 +0900

    깃 브랜치 연습

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,60 +1,32 @@
-// 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
-// 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
-// 코드 예시를 남겨 두었습니다.
-
-import * as Api from '/api.js';
-import { randomId } from '/useful-functions.js';
-
 // 요소(element), input 혹은 상수
-const landingDiv = document.querySelector('#landingDiv');
-const greetingDiv = document.querySelector('#greetingDiv');
+const cards = document.getElementsByClassName('card')
 
 addAllElements();
 addAllEvents();
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
-  insertTextToLanding();
-  insertTextToGreeting();
 }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  landingDiv.addEventListener('click', alertLandingText);
-  greetingDiv.addEventListener('click', alertGreetingText);
+  cards[1].addEventListener('mouseover', cardMouseover1)
+  cards[1].addEventListener('mouseout', cardMouseout1)
+  cards[2].addEventListener('mouseover', cardMouseover2)
+  cards[2].addEventListener('mouseout', cardMouseout2)
 }
 
-function insertTextToLanding() {
-  landingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-    `
-  );
+function cardMouseover1(e) {
+  cards[0].classList.remove('spread')
+  cards[1].classList.add('spread')
 }
-
-function insertTextToGreeting() {
-  greetingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-    `
-  );
+function cardMouseout1(e) {
+  cards[0].classList.add('spread')
+  cards[1].classList.remove('spread')
 }
-
-function alertLandingText() {
-  alert('n팀 쇼핑몰입니다. 안녕하세요.');
+function cardMouseover2(e) {
+  cards[0].classList.remove('spread')
+  cards[2].classList.add('spread')
 }
-
-function alertGreetingText() {
-  alert('n팀 쇼핑몰에 오신 것을 환영합니다');
-}
-
-async function getDataFromApi() {
-  // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-  const data = await Api.get('/api/user/data');
-  const random = randomId();
-
-  console.log({ data });
-  console.log({ random });
+function cardMouseout2(e) {
+  cards[0].classList.add('spread')
+  cards[2].classList.remove('spread')
 }
