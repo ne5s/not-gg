e80f47c38916021a1f1b2b8ad711c563020f010d
YONGHWAN <younghwan101@gmail.com>
2022-06-29 04:03:23 +0900

    랭킹 js 작업

--- src/views/ranking/ranking.js
+++ src/views/ranking/ranking.js
@@ -1,83 +1,17 @@
 import * as Api from '/api.js';
 
-const duoFormsBox = document.getElementById('duoFormsBox');
-console.log(duoFormsBox);
-addAllElements();
+soloRouter.get('/soloRanking', loginRequired, async function (req, res, next) {
+	try {
+		// 전체 사용자 목록(랭킹으로 sort된) 얻음
+		const users = await summonerSoloService.getUsersForRanking();
 
-const duoDatas = await Api.get('/api/duo?page=0');
-console.log(duoDatas);
-makeDuo();
-
-function makeDuo() {
-	duoDatas.forEach((data) => {
-		const duoComment = data.DuoComment;
-		const MainPosition = data.MainPosition;
-		const myTier = data.MyTier;
-		const searchPosition = data.SearchPosition;
-		const searchTier = data.SearchTier;
-		const summonerName = data.SummonerName;
-		const createdAt = data.createdAt;
-
-		let tierIcon = '';
-		switch (MainPosition) {
-			case 'TOP':
-				tierIcon = '탑';
-				break;
-			case 'JUNGLE':
-				tierIcon = '정글';
-				break;
-			case 'MIDDLE':
-				tierIcon = '미드';
-				break;
-			case 'BOTTOM':
-				tierIcon = '원딜';
-				break;
-			case 'UTILITY':
-				tierIcon = '서폿';
-				break;
-		}
-
-		duoFormsBox.innerHTML += `
-            <ul class="duo-forms-item-box">
-                <li>
-                    <span class="components">아파요 머리가</span>
-                    <span>님이</span>
-                    <span class="components">${searchTier} ${searchPosition}</span>
-                    <span>를 찾고 있습니다</span>
-                </li>
-                <li>
-                    <img src="../img/${tierIcon}.png" alt="">
-                </li>
-                <li>${myTier}</li>
-                <li>
-                    <span class="comment">${duoComment}</span>
-                </li>
-                <li>1분전</li>
-                <li>
-                    <div class="btn--delete">
-                        <span class="material-icons">
-                        clear
-                        </span>
-                    </div>
-                </li>
-            </ul>
-        `;
-	});
-}
-
-async function addAllElements() {
-	getDuoDatas();
-}
-
-async function getDuoDatas() {
-	//듀오 폼 가져와서
-	//renderDuoForms으로 forEach
-}
-
-function renderDuoData(object) {
-	//듀오 폼 하나하나 렌더링
-	const DuoComment = duoDatas;
-}
+		// 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
+		res.status(200).json(users);
+	} catch (error) {
+		next(error);
+	}
+});
+console.log(users);
 
 // topbutton
 let topBtn = document.querySelector('.top-circle');
