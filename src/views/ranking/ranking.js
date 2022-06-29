171c5afb62b5d814248af8f169d21ade26d85184
YONGHWAN <younghwan101@gmail.com>
2022-06-29 21:42:09 +0900

    랭킹 페이지 제작중

--- src/views/ranking/ranking.js
+++ src/views/ranking/ranking.js
@@ -1,43 +1,51 @@
 import * as Api from '/api.js';
 
-const fetchProductList = async () => {
+const fetchRankingList = async () => {
 	try {
-	  const productList = await Api.get('/api/product/list');
-document.querySelector('#producItemContainer').insertAdjacentHTML(
-	'afterbegin',
-	`${productList
-	  .map(
-		(product) =>
-		  `
-		  <div class="message product-item imageSort" data-category-name="${
-			product.productCategory
-		  }" data-product-name="${product.productName}">
-			<div class="media-left">
-			  <figure class="image">
-				<img src="${product.productImage}" alt="제품 이미지" />
-			  </figure>
-			</div>
-			<div class="media-content">
-				<div class="content">
-				  <p class="title">
-					${product.productName}
-				  </p>
-				  <p class="description">
-					${product.productShortDes}
-				  </p>
-				  <p class="price">${addCommas(product.productPrice)}원</p>
-				</div>
-			  </div>  
-			</div>
-		  `
-	  )
-	  .join('')}`
-  );
-} catch (err) {
-  console.error(err.stack);
-  alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-}
+		const RankingList = await Api.get('/api/soloRanking');
+		document.querySelector('#Rank-item-table').insertAdjacentHTML(
+			'afterbegin',
+			`${RankingList
+				.map(
+					(ranking) =>
+						`
+						<tr>
+						<th scope="row">1</th>
+						<td class="summoner-name">어리고 싶다</td>
+						<td>Challenger</td>
+						<td>1144 LP</td>
+						<td>
+							<div class="most3">
+								<div class="circle"></div>
+								<div class="circle"></div>
+								<div class="circle"></div>
+							</div>
+						</td>
+						<td>56%</td>
+					</tr>
+				`,
+				)
+				.join('')}`,
+		);
+	} catch (err) {
+		console.error(err.stack);
+		alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+	}
 };
+
+// 소환사 이름 클릭시 상세 페이지로 이동
+const attachEvent = () => {
+	const items = document.querySelectorAll('.summoner-name');
+	items.forEach((item) => {
+		item.addEventListener('click', function () {
+			const sumomonerName = this.getAttribute('data-product-name');
+			window.location.href = `/product_detail/?name=${productName}`;
+		});
+	});
+};
+
+await fetchRankingList();
+attachEvent();
 
 // topbutton
 let topBtn = document.querySelector('.top-circle');
